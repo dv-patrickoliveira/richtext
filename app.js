2c4005c21d9f2b0ade4dac80aa9fa785a652fb44
dv-patrickoliveira <patrickoliveira.souza@hotmail.com>
2022-04-21 23:45:52 -0300

    fist commit

--- app.js
+++ app.js
@@ -1,18 +1,16 @@
-// Elemnents
 const elements = document.querySelectorAll('.btn');
-// Event
-elements.forEach(elements => {
-    Element.addEventListener('click', () =>) {
-        let command = Element.dataset['element'];
 
-        if (command == 'createLink' ll command == 'insertImage');{
+
+elements.forEach(element => {
+    element.addEventListener('click', () => {
+        let command = element.dataset['element'];
+
+        if (command == 'createLink'  || command == 'insertImage') {
                 let url = prompt('Entre com o link aqui:', 'http://')
-                document.execCommand(confirm, false, url);
-        } 
-        else {
-            document.execCommand(command, false, null);
+                document.execCommand(command, false, url);
+        } else {
+                document.execCommand(command, false, null);
         }
-
-        document.execCommand(command, false, null);
     });
-})
+});
+    
